d48fa8d0c8fcf63314cab1889d55963cff7d940b
natasha <kucera.nat@gmail.com>
2023-02-08 21:48:49 -0500

    Made site more mobile friendly

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,71 +1,70 @@
-import React, { Component } from "react";
-import { Link, animateScroll as scroll } from "react-scroll";
+import React from "react";
+import { Link } from "react-scroll";
+import { useState } from "react";
+import Hamburger from 'hamburger-react';
 
 
-export default class Navbar extends Component {
+export default function Navbar() {
 
-  scrollToTop = () => {
-    scroll.scrollToTop();
-  };
-  
+  const [showNavbar, setShowNavbar] = useState(false)
 
-  render() {
+  const handleShowNavbar = () => {
+    setShowNavbar(!showNavbar)
+  }
+
     return (
       <nav className="nav" id="navbar">
-        <ul className="nav-items">
-          <li className="nav-item">
-            <Link
-              activeClass="active"
-              to="aboutme"
-              spy={true}
-              smooth={true}
-              offset={-70}
-              duration={500}
-            >
-              About Me
-            </Link>
-          </li>
+          <div className="menu-icon" onClick={handleShowNavbar}>
+            <Hamburger />
+          </div>
+          <div className={`nav-elements  ${showNavbar && 'active'}`}>
+            <ul className="nav-items">
+              <li className="nav-item">
+                <Link
+                  activeClass="active"
+                  to="aboutme"
+                  spy={true}
+                  smooth={true}
+                  offset={-70}
+                  duration={500}
+                >About Me</Link>
+              </li>
 
-          <li className="nav-item">
-            <Link
-              activeClass="active"
-              to="projects"
-              spy={true}
-              smooth={true}
-              offset={-70}
-              duration={500}
-            >
-              Projects
-            </Link>
-          </li>
+              <li className="nav-item">
+                <Link
+                  activeClass="active"
+                  to="projects"
+                  spy={true}
+                  smooth={true}
+                  offset={-70}
+                  duration={500}
+                >Projects</Link>
+              </li>
 
-          <li className="nav-item">
-            <Link
-              activeClass="active"
-              to="skills"
-              spy={true}
-              smooth={true}
-              offset={-70}
-              duration={500}
-            >
-              Skills
-            </Link>
-          </li>
-          
-          <li className="nav-item">
-            <Link
-              activeClass="active"
-              to="extra"
-              spy={true}
-              smooth={true}
-              offset={-70}
-              duration={500}
-            >
-              Extra
-            </Link>
-          </li>
-        </ul>
+              <li className="nav-item">
+                <Link
+                  activeClass="active"
+                  to="skills"
+                  spy={true}
+                  smooth={true}
+                  offset={-70}
+                  duration={500}
+                >Skills</Link>
+              </li>
+              
+              <li className="nav-item">
+                <Link
+                  activeClass="active"
+                  to="extra"
+                  spy={true}
+                  smooth={true}
+                  offset={-70}
+                  duration={500}
+                >Extra</Link>
+              </li>
+            </ul>
+
+        </div>
       </nav>
     );
   }
-}
